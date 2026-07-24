bfc965793a25e4a4196d5ca5ff054e8411e4ad57
janestreet1112 <janestreet1112@gmail.com>
2026-07-24 18:58:08 +0530

    Updated and tested app locally

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-import os
 import streamlit as st
 
-def get_secret(key):
-    """Fetches secrets from OS environment variables (Render) or Streamlit secrets."""
-    return os.environ.get(key) or st.secrets.get(key)
-
-# Read credentials
-api_key = get_secret("SMARTAPI_API_KEY")
-client_id = get_secret("SMARTAPI_USERNAME")
-pin = get_secret("SMARTAPI_PASSWORD")
-totp_secret = get_secret("SMARTAPI_TOTP_KEY")
+st.set_page_config(page_title="NIFTY Predictor", layout="wide")
+st.title("⚡ NIFTY 50 Predictor Dashboard")
+st.write("If you can see this, Streamlit is working!")
